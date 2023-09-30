97de72d20211ed777e7ad0f8f0a92cf94b4b6afa
César Cadillo <gkktemporal@gmail.com>
2023-09-30 09:17:15 -0500

    Proyecto VisualStudio

---
+++ Taller3_Prom1_Examen_CesarCadillo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3_Prom1_Examen_CesarCadillo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
